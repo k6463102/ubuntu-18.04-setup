7d6eb1f66816b98b951a603ff6d91023520f3e64
Ikeda Ryo <k6463102@kadai.jp>
2021-02-19 12:02:38 +0900

    add: gnuplotのインストールを追加

--- install-standard.sh
+++ install-standard.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 sudo apt-get install build-essential vim tmux git manpages-ja manpages-ja-dev gdebi -y
-sudo apt-get install htop tree nkf curl filezilla gnome-tweaks python3-venv gimp gimp-help-ja -y
+sudo apt-get install htop tree nkf curl filezilla gnome-tweaks python3-venv gimp gimp-help-ja gnuplot -y
 sudo apt-get install blubuntu-wallpapers edubuntu-wallpapers xubuntu-wallpapers -y
 
 sudo apt-get autoremove -y
